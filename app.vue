1d25035432b701369d5328104221a2dd705c3ae8
ajiken4610 <86188770+ajiken4610@users.noreply.github.com>
2022-08-19 23:19:24 +0900

    テスト

--- app.vue
+++ app.vue
@@ -5,9 +5,10 @@
 </template>
 <script setup lang="ts">
 const dataflow = newDataflow({ 1: 2, 3: "4" }, { 5: 6, 7: "8" });
-const child = dataflow.add({ 10: 11 }, {});
-child.emit(1, 3);
 
+const child = dataflow.add({ 10: 11 }, {});
+const child2 = child.add({}, {});
+child.emit(3, "hello");
 console.log(dataflow);
-console.log(child.inject(5));
+console.log(child2.inject(7));
 </script>
