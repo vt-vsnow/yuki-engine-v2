69891ef803a8b307baea344f832515b5dcf14de5
ajiken4610 <86188770+ajiken4610@users.noreply.github.com>
2022-08-20 20:17:47 +0900

    useRenderflowを実装

--- composables/RenderDataflow.ts
+++ composables/RenderDataflow.ts
@@ -3,9 +3,9 @@ const renderDataflowProps = {
   loading: 0,
   suspending: 0,
   count: 0,
-  updateLoading: null as null | [string, number],
-  updateSuspending: null as null | [string, number],
-  updateCount: null as null | [string, number],
+  loadings: {} as { [key: string]: number },
+  suspendings: {} as { [key: string]: number },
+  counts: {} as { [key: string]: number },
 };
 const renderDataflowProvides = {};
 type RenderDataflowProps = typeof renderDataflowProps;
@@ -16,15 +16,81 @@ type ToplevelRenderDataflow<PROPS, PROVIDES> = Dataflow<
   {},
   {}
 >;
-const topLevelhandle = <PROPS, PROVIDES>(
-  self: ToplevelRenderDataflow<PROPS, PROVIDES>
-) => {};
+type RenderDataflow<PROPS, PROVIDES, EMITS, INJECTS> = Dataflow<
+  RenderDataflowProps & PROPS,
+  RenderDataflowProvides & PROVIDES,
+  EMITS,
+  INJECTS
+>;
+const objectReduce = (
+  o: { [key: string]: number },
+  fn: (sum, val, key) => number
+) => {
+  let sum = 0;
+  for (const key in o) {
+    sum = fn(sum, o[key], key);
+  }
+  return sum;
+};
+const handle = <PROPS, PROVIDES>(
+  self:
+    | ToplevelRenderDataflow<PROPS, PROVIDES>
+    | Dataflow<
+        RenderDataflowProps & PROPS,
+        RenderDataflowProvides & PROVIDES,
+        RenderDataflowProps & PROPS,
+        RenderDataflowProvides & PROVIDES
+      >
+) => {
+  const { loading, suspending, count, loadings, suspendings, counts } = toRefs(
+    self.props
+  );
+  // loading
+  watch(loadings, (val, old) => {
+    // ループしてしまうんだが。
+    loadings.value = old;
+    for (const key in val) {
+      //
+    }
+  });
+  watchEffect(() => {
+    if (loading === -1) {
+      for (const key in loadings.value) {
+        loadings[key] = -1;
+      }
+    }
+  });
+
+  // 集計
+  watchEffect(() => {
+    loading.value = objectReduce(
+      loadings.value,
+      (sum, val, key) => sum + (val === -1 ? counts[key] : val)
+    );
+  });
+};
 
 export const useRenderDataflow = <PROPS, PROVIDES>(
   props: PROPS,
-  provides: PROVIDES
-): ToplevelRenderDataflow<PROPS, PROVIDES> =>
-  new Dataflow(
-    { ...renderDataflowProps, ...props },
-    { ...renderDataflowProvides, ...provides }
-  );
+  provides: PROVIDES,
+  parent: RenderDataflow<
+    PROPS,
+    PROVIDES,
+    RenderDataflowProps,
+    RenderDataflowProvides
+  >
+) => {
+  if (parent) {
+    return parent.addChild(
+      { ...renderDataflowProps, ...props },
+      { ...renderDataflowProvides, ...provides },
+      handle
+    );
+  } else {
+    return new Dataflow(
+      { ...renderDataflowProps, ...props },
+      { ...renderDataflowProvides, ...provides },
+      handle
+    );
+  }
+};
