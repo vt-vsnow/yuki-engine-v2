c27464d789dfc621a290c5b1d35ff0b52616df3b
ajiken4610 <86188770+ajiken4610@users.noreply.github.com>
2022-08-20 19:07:30 +0900

    useRenderDataflowを定義

--- composables/RenderDataflow.ts
+++ composables/RenderDataflow.ts
@@ -1,1 +1,30 @@
-export const useRenderDataflow = () => {};
+import { Dataflow } from "./Dataflow";
+const renderDataflowProps = {
+  loading: 0,
+  suspending: 0,
+  count: 0,
+  updateLoading: null as null | [string, number],
+  updateSuspending: null as null | [string, number],
+  updateCount: null as null | [string, number],
+};
+const renderDataflowProvides = {};
+type RenderDataflowProps = typeof renderDataflowProps;
+type RenderDataflowProvides = typeof renderDataflowProvides;
+type ToplevelRenderDataflow<PROPS, PROVIDES> = Dataflow<
+  RenderDataflowProps & PROPS,
+  RenderDataflowProvides & PROVIDES,
+  {},
+  {}
+>;
+const topLevelhandle = <PROPS, PROVIDES>(
+  self: ToplevelRenderDataflow<PROPS, PROVIDES>
+) => {};
+
+export const useRenderDataflow = <PROPS, PROVIDES>(
+  props: PROPS,
+  provides: PROVIDES
+): ToplevelRenderDataflow<PROPS, PROVIDES> =>
+  new Dataflow(
+    { ...renderDataflowProps, ...props },
+    { ...renderDataflowProvides, ...provides }
+  );
