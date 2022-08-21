89223d6874d5e1bf82e9290514e3a68102f86504
ajiken4610 <86188770+ajiken4610@users.noreply.github.com>
2022-08-21 13:13:45 +0900

    Dataflowにコメント付け

--- composables/Dataflow.ts
+++ composables/Dataflow.ts
@@ -4,16 +4,34 @@ type DataflowHandle<PROPS, PROVIDES, EMITS, INJECTS> = (
   flow: Dataflow<PROPS, PROVIDES, EMITS, INJECTS>
 ) => void;
 
+/**
+ * 上下方向へのデータのフローを提供します。
+ */
 export class Dataflow<
   PROPS extends {},
   PROVIDES extends {},
   EMITS extends {},
   INJECTS extends {}
 > {
+  /**
+   * このフローが保持するProps
+   */
   public readonly props: PROPS;
+  /**
+   * このフローが保持するProvides
+   */
   public readonly provides: PROVIDES;
+  /**
+   * 上のフローへのEmit
+   */
   public readonly emit: EmitFunc<EMITS>;
+  /**
+   * 上のフローからのInject
+   */
   public readonly inject: InjectFunc<INJECTS>;
+  /**
+   * props,providesと、handle,emit,inject関数(optional)からインスタンスを初期化します。
+   */
   constructor(
     props: PROPS,
     provides: PROVIDES,
@@ -27,15 +45,24 @@ export class Dataflow<
     this.inject = inject;
     handle && handle(this);
   }
+  /**
+   * このインスタンスのpropsを変更するemit関数を返します。
+   */
   getEmitfunc(): EmitFunc<PROPS> {
     return (e, val) => {
       this.props[e] = val;
     };
   }
+  /**
+   * このインスタンスおよびその上のフローからのprovideをinjectする関数を返します。
+   */
   getInjectfunc(): InjectFunc<PROVIDES & INJECTS> {
     // @ts-ignore
     return (e) => this.provides[e] || this.inject(e);
   }
+  /**
+   * 新しい子フローを作成して返します。
+   */
   addChild<CHILD_PROPS, CHILD_PROVIDES>(
     props: CHILD_PROPS,
     provides: CHILD_PROVIDES,
