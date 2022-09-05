89c08ea81eedbfd5dd92d4bdb658636f04dfabed
ajiken4610 <86188770+ajiken4610@users.noreply.github.com>
2022-09-05 11:51:48 +0900

    importに変更

--- pages/test/viewport.vue
+++ pages/test/viewport.vue
@@ -1,12 +1,17 @@
 <template lang="pug">
 div
-  CoreViewport
+  Viewport
     template(#3d="{}")
-      CoreGroup
-        CoreMeshGLTF(v-bind="{ path }")
-        CoreMeshCube(v-bind="{ dx: -6, dy: 1, dz: 1 }")
+      Group
+        GLTF(v-bind="{ path }")
+        Cube(v-bind="{ dx: -6, dy: 1, dz: 1 }")
 </template>
 
 <script setup lang="ts">
+import Viewport from "@/components/core/Viewport.vue";
+import Group from "@/components/core/Group.vue";
+import GLTF from "@/components/core/mesh/GLTF.vue";
+import Cube from "@/components/core/mesh/Cube.vue";
+
 const path: string = (await import("/assets/部屋.glb?url")).default;
 </script>
