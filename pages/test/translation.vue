674f0573db113b5a5f1e96921b45565a5174f093
ajiken4610 <ajiken4610@gmail.com>
2023-02-02 19:41:37 +0900

    ひとまず完成

---
+++ pages/test/translation.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+div
+  CoreViewport(v-model:flow="flow")
+    template(#2d="{}")
+      textarea(v-model="translationSet.src" style="width:90%;height:10rem")
+      div {{ translationSet.dst }}
+</template>
+
+<script setup lang="ts">
+const flow = ref<ReturnType<typeof useRenderDataflow>>();
+const translationSet = useTranslation()
+translationSet.src = "翻訳する文章を入力"
+</script>
